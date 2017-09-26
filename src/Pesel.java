73617775b1cbeedb93d91cea131c362d6771d0f0
Bartek3 <pl.kasspee@gmail.com>
2017-09-27 00:33:37 +0200

    Added comments

--- src/Pesel.java
+++ src/Pesel.java
@@ -4,20 +4,20 @@ public class Pesel {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] Pesel = input(in); //Pobranie numeru PESEL w postaci tablicy cyfr
-        if (checkCorrectnessOfPesel(Pesel)) { //Sprawdzenie, czy numer PESEL jest poprawny (z pomocą odpowiedniej metody)
+        int[] pesel = input(in); //Pobranie numeru PESEL w postaci tablicy jego poszczególnych cyfr
+        if (checkCorrectnessOfPesel(pesel)) { //Sprawdzenie, czy numer PESEL jest poprawny (z pomocą odpowiedniej metody)
             System.out.println("This PESEL number is correct!");
         } else {
             System.out.println("This PESEL number is incorrect!");
         }
     }
 
-    // Metoda pobierająca numer PESEL, mający koniecznie 11 cyfr
+    //Metoda pobierająca numer PESEL, mający koniecznie 11 cyfr
     private static int[] input(Scanner in) {
 
         String input = in.next();
 
-        // Warunek, że PESEL musi mieć 11 znaków
+        //Warunek, że PESEL musi mieć 11 znaków
         if (input.length() != 11) {
             System.out.println("Input variable is not a valid PESEL number!");
             System.exit(0);
@@ -25,7 +25,7 @@ public class Pesel {
 
         char[] inputArray = input.toCharArray();
 
-        // Warunek, że każdy znak numeru PESEL musi być cyfrą
+        //Warunek, że każdy znak numeru PESEL musi być cyfrą
         for (int i = 0; i < 11; i++) {
             if (!Character.isDigit(inputArray[i])) {
                 System.out.println("Input variable is not a valid PESEL number!");
@@ -33,16 +33,16 @@ public class Pesel {
             }
         }
 
-        // Konwersja tablicy znaków na tablicę integerów
+        //Konwersja tablicy znaków na tablicę integerów
         int[] pesel = new int[11];
         for (int i = 0; i < 11; i++) {
             pesel[i] = Character.getNumericValue(inputArray[i]);
         }
 
-        return pesel; // Zwrócenie tablicy integerów zawierającej numer PESEL
+        return pesel; //Zwrócenie tablicy integerów zawierającej numer PESEL
     }
 
-    // Metoda sprawdzająca poprawność wprowadzonego numeru PESEL
+    //Metoda sprawdzająca poprawność wprowadzonego numeru PESEL
     private static boolean checkCorrectnessOfPesel(int[] pesel) {
         int controlNumber = (pesel[0]
                 + pesel[1] * 3
@@ -56,7 +56,7 @@ public class Pesel {
                 + pesel[9] * 3
                 + pesel[10]);
 
-        // Jeśli PESEL jest poprawny, to utworzona liczba jest podzielna przez 10
+        //Jeśli PESEL jest poprawny, to utworzona liczba jest podzielna przez 10
         if (controlNumber % 10 == 0) {
             return true;
         }
