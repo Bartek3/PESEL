0a89c5ea64d7104a7bf4f790ea1342e40150116d
Bartek <pl.kasspee@gmail.com>
2018-04-11 21:54:40 +0200

    Uproszczenie kodu

--- src/Pesel.java
+++ src/Pesel.java
@@ -1,66 +1,35 @@
 import java.util.Scanner;
 
-public class Pesel {
+public class Main {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int[] pesel = input(in); //Pobranie numeru PESEL w postaci tablicy jego poszczególnych cyfr
-        if (checkCorrectnessOfPesel(pesel)) { //Sprawdzenie, czy numer PESEL jest poprawny (z pomocą odpowiedniej metody)
-            System.out.println("This PESEL number is correct!");
+        System.out.println("Wprowadź numer PESEL składający się z 11 cyfr.");
+        int controlNumber = 0;
+        int[] controlMultipliers = new int[]{1, 3, 7, 9, 1, 3, 7, 9, 1, 3, 1};
+        Long input = 0L;
+
+        if (in.hasNextLong()) {
+            input = in.nextLong();
         } else {
-            System.out.println("This PESEL number is incorrect!");
+            System.out.println("Wprowadzono nieprawidłowy numer PESEL!");
+            System.exit(1);
         }
-    }
+        String inputString = input.toString();
 
-    //Metoda pobierająca numer PESEL, mający koniecznie 11 cyfr
-    private static int[] input(Scanner in) {
-
-        String input = in.next();
-
-        //Warunek, że PESEL musi mieć 11 znaków
-        if (input.length() != 11) {
-            System.out.println("Input variable is not a valid PESEL number!");
-            System.exit(0);
+        if (inputString.length() != 11) {
+            System.out.println("Wprowadzono numer o nieprawidłowej długości!");
+            System.exit(1);
         }
 
-        char[] inputArray = input.toCharArray();
-
-        //Warunek, że każdy znak numeru PESEL musi być cyfrą
         for (int i = 0; i < 11; i++) {
-            if (!Character.isDigit(inputArray[i])) {
-                System.out.println("Input variable is not a valid PESEL number!");
-                System.exit(0);
-            }
+            controlNumber += Character.getNumericValue(inputString.charAt(i)) * controlMultipliers[i];
         }
 
-        //Konwersja tablicy znaków na tablicę integerów
-        int[] pesel = new int[11];
-        for (int i = 0; i < 11; i++) {
-            pesel[i] = Character.getNumericValue(inputArray[i]);
-        }
-
-        return pesel; //Zwrócenie tablicy integerów zawierającej numer PESEL
-    }
-
-    //Metoda sprawdzająca poprawność wprowadzonego numeru PESEL
-    private static boolean checkCorrectnessOfPesel(int[] pesel) {
-        int controlNumber = (pesel[0]
-                + pesel[1] * 3
-                + pesel[2] * 7
-                + pesel[3] * 9
-                + pesel[4]
-                + pesel[5] * 3
-                + pesel[6] * 7
-                + pesel[7] * 9
-                + pesel[8]
-                + pesel[9] * 3
-                + pesel[10]);
-
-        //Jeśli PESEL jest poprawny, to utworzona liczba jest podzielna przez 10
         if (controlNumber % 10 == 0) {
-            return true;
+            System.out.println("Numer PESEL jest poprawny!");
+        } else {
+            System.out.println("Numer PESEL nie jest poprawny!");
         }
-
-        return false;
     }
 }
